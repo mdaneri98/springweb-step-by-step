eb2500d909201638ead7ce82c319a7aad84fb992
matiasdaneri <mdaneri@itba.edu.ar>
2024-08-20 15:07:54 -0300

    Working with query params.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -4,7 +4,9 @@ import ar.edu.itba.paw.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ViewResolver;
 
@@ -22,9 +24,18 @@ public class HelloWorldController {
     }
 
     @RequestMapping("/")
-    public ModelAndView helloWorld() {
+    public ModelAndView index(@RequestParam(name = "userId", defaultValue = "1") long userId) {
         final ModelAndView mav = new ModelAndView("index");
-        mav.addObject("greeting", userService.findById(1).get().getUsername());
+        mav.addObject("userId", userId);
+        mav.addObject("username", userService.findById(userId).get().getUsername());
+        return mav;
+    }
+
+    @RequestMapping("/{userId:\\d+}")
+    public ModelAndView profile(@PathVariable(name = "userId") long userId) {
+        final ModelAndView mav = new ModelAndView("profile");
+        mav.addObject("userId", userId);
+        mav.addObject("username", userService.findById(userId).get().getUsername());
         return mav;
     }
 
--- webapp/src/main/webapp/WEB-INF/jsp/profile.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/profile.jsp
@@ -1,6 +1,8 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt"%>
 <html>
 <body>
+
+<h1>User profile</h1>
 <h2>Hello <c:out value="${username}"/>!</h2>
 
 <h5>Your user id is <c:out value="${userId}"/></h5>
