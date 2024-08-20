af27f73492598f7d09d0084cd8011b770d20eaf1
matiasdaneri <mdaneri@itba.edu.ar>
2024-08-20 12:19:20 -0300

    As dependencies are transitive, now services can also use models.

--- services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
@@ -1,4 +1,15 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.User;
+
+import java.util.Optional;
+
 public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public Optional<User> findById(long id) {
+        return Optional.empty();
+    }
+
 }
