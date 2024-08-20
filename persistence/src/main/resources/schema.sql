7f60ac17e2161996b4ece045f7ad4eec06c19d12
matiasdaneri <mdaneri@itba.edu.ar>
2024-08-20 16:13:26 -0300

    added docker for using an example postgresql database

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -2,3 +2,5 @@ CREATE TABLE IF NOT EXISTS users (
   userId SERIAL PRIMARY KEY,
   username varchar(255) UNIQUE NOT NULL
 );
+
+INSERT INTO users (username) VALUES ('new_user');
--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,10 +1,15 @@
 package ar.edu.itba.paw.webapp.config;
 
 import ar.edu.itba.paw.services.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -27,6 +32,9 @@ import javax.sql.DataSource;
 @Configuration
 public class WebConfig {
 
+    @Value("classpath:schema.sql")
+    private Resource schemaSql;
+
     @Bean
     public ViewResolver viewResolver() {
         final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -41,11 +49,25 @@ public class WebConfig {
         final SimpleDriverDataSource ds = new SimpleDriverDataSource();
 
         ds.setDriverClass(org.postgresql.Driver.class);
-        ds.setUrl("jdbc:postgresql://localhost/paw");
+        ds.setUrl("jdbc:postgresql://localhost:5432/test_db");
         ds.setUsername("root");
         ds.setPassword("root");
 
         return ds;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
+        DataSourceInitializer dsi = new DataSourceInitializer();
+        dsi.setDataSource(ds);
+        dsi.setDatabasePopulator(dsPopulator());
+        return dsi;
+    }
+
+    public DatabasePopulator dsPopulator() {
+        ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
+        dbp.addScript(schemaSql);
+        return dbp;
     }
 
 }
