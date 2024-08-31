829cd7358475c9e8f25669748b50ae283e22399d
matiasdaneri <mdaneri@itba.edu.ar>
2024-08-31 12:16:32 -0300

    Basic test

--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -1,7 +1,56 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.User;
+import ar.edu.itba.paw.persistence.UserDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.dao.DuplicateKeyException;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+
+
 public class UserServiceImplTest {
 
-    
+    private static final String USERNAME = "username";
+    private UserServiceImpl userService;
+
+    private UserDao mock;
+
+    @Before
+    public void setUp() {
+        mock = Mockito.mock(UserDao.class);
+        this.userService = new UserServiceImpl(mock);
+    }
+
+    @Test
+    public void testCreate() {
+        // 1. Precondiciones.
+        Mockito.when(mock.create(eq(USERNAME))).thenReturn(new User(1, USERNAME));
+
+        // 2. Ejercito la clase under test.
+        User user = userService.create(USERNAME);
+
+        // 3. Valido las postcondiciones
+        assertNotNull(user);
+        assertEquals(USERNAME, user.getUsername());
+
+    }
+
+    @Test(expected = DuplicateKeyException.class)
+    public void testCreateDuplicate() {
+        // 1. Precondiciones.
+        Mockito.when(mock.create(eq(USERNAME))).thenThrow(DuplicateKeyException.class);
+
+        // 2. Ejercito la clase under test.
+        User user = userService.create(USERNAME);
+
+        // 3. Valido las postcondiciones
+        fail();
+    }
 
 }
