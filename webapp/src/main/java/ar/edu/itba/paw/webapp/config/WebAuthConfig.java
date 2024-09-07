9f484ec1da97009cb0783dcc71069f5857f14ce9
matiasdaneri <mdaneri@itba.edu.ar>
2024-09-07 12:50:56 -0300

    Added security to webapp and done basic WebAuthConfig

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -1,10 +1,45 @@
 package ar.edu.itba.paw.webapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.concurrent.TimeUnit;
+
+@EnableWebSecurity /* Default de web security */
 @Configuration
-public class WebAuthConfig {
+public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
-    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .invalidSessionUrl("/login")
+                .and().authorizeRequests()
+                .antMatchers("/create", "/login").anonymous()
+                .antMatchers("/profile").hasRole("USER")
+                .antMatchers("/post/edit").hasRole("EDITOR")
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginPage("/login")
+                .defaultSuccessUrl("/", false)
+                .and().rememberMe()
+                .rememberMeParameter("remember_me")
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+                .and().logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .and().exceptionHandling()
+                .accessDeniedPage("/403")
+                .and().csrf().disable();
+    }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "favicon.ico", "/403");
+    }
 }
